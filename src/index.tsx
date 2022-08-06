a392c2b3f4dd8933e7502038628b0e5fefe55bb3
anttibull <andriibu@wix.com>
2022-08-06 14:34:49 +0300

    feat: add GraphQl & Apollo
    [CASH-4]

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, { FC } from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery, ApolloLink, concat, HttpLink } from '@apollo/client';
 
 import "./index.css";
 
-const element  =  <h1>Hi</h1>
+const EXPENSE_REQUEST = gql`
+  query {
+    expense( id: 1){
+      data {
+        attributes {
+          title
+        }
+      }
+    }
+  }
+`
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+const token = process.env.REACT_APP_API_TOKEN
+
+  operation.setContext({
+    headers: {
+      "Authorization": token ? `Bearer ${token}` : "",
+    },
+  });
+
+  return forward(operation);
+});
+
+
+const client = new ApolloClient({
+  link: concat(authMiddleware, new HttpLink({ uri: process.env.REACT_APP_API})),
+  cache: new InMemoryCache(),
+});
+
+const App: FC = () => {
+  const { loading, error, data } = useQuery(EXPENSE_REQUEST);
+
+
+  if(loading){
+    return <span>loading</span>
+  }
+
+  if(error){
+    return <h1>Error, Нюхай бебру</h1>
+  }
+
+  return  <h1>Expense: {data.expense.data ?? 'not found, нюхай бебру :('}</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-   {element}
+    <ApolloProvider client={client}>
+      <App/>
+    </ApolloProvider>
   </React.StrictMode>
-);
+)
